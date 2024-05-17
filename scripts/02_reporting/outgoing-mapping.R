6507e28d1313fe0496535bb50437203b05201caf
almac2022 <al@newgraphenvironment.com>
2024-05-17 09:04:30 -0700

    simplify and update to build objects for 2023

--- scripts/02_reporting/outgoing-mapping.R
+++ scripts/02_reporting/outgoing-mapping.R
@@ -14,64 +14,60 @@ fpr_make_geopackage(dat = phase1_priorities)
 
 
 ##add the tracks
-sf::read_sf("./data/habitat_confirmation_tracks.gpx", layer = "tracks") %>%
-  sf::st_write(paste0("./data/fishpass_mapping/", 'fishpass_mapping', ".gpkg"), 'hab_tracks', append = TRUE)
+sf::read_sf("data/habitat_confirmation_tracks.gpx", layer = "tracks") %>%
+  sf::st_write("data/fishpass_mapping/fishpass_mapping.gpkg", 'hab_tracks', append = TRUE)
 
-##study area watersheds
-conn <- DBI::dbConnect(
-  RPostgres::Postgres(),
-  dbname = Sys.getenv('PG_DB_DO'),
-  host = Sys.getenv('PG_HOST_DO'),
-  port = Sys.getenv('PG_PORT'),
-  user = Sys.getenv('PG_USER_DO'),
-  password = Sys.getenv('PG_PASS_DO')
-)
-
-# dbGetQuery(conn,
-#            "SELECT column_name,data_type
-#            FROM information_schema.columns
-#            WHERE table_name='fwa_watershed_groups_poly'")
-
-##here is the study area watersheds
-wshd_study_areas <- st_read(conn,
-                           query = "SELECT * FROM whse_basemapping.fwa_watershed_groups_poly a
-                               WHERE a.watershed_group_code IN ('ZYMO','MORR','KISP')"
+wshd_study_areas <- fpr_db_query(
+  query = "SELECT * FROM whse_basemapping.fwa_watershed_groups_poly a
+                               WHERE a.watershed_group_code IN ('ZYMO','MORR','KISP', 'BULK', 'KLUM')"
 )
 
 wshd_study_areas %>%
   # select(-wscode_ltree) %>%
   st_cast("POLYGON") %>%
   sf::st_transform(crs = 4326) %>%
-  sf::st_write(paste0("./data/fishpass_mapping/", 'fishpass_mapping', ".gpkg"), 'wshd_study_areas', append = F)
+  sf::st_write("data/fishpass_mapping/fishpass_mapping.gpkg", 'wshd_study_areas', append = F)
 
-dbDisconnect(conn = conn)
 
 ####------------add the watersheds-------------------------
 
-##we needed to remove crossings that are first order - this used to run but doesn't want to anymore
-##i wonder if it is because the 1st order watershed is the first one on the list so the api kicks us off...
+##we need to remove crossings that are first order - b/capi kicks us off...
+# we don't need to do that in 2023 bc all are 2nd order or higher
+# !if we have 1st order watersheds see procedures in skeena 2022 script
 bcfishpass_phase2_clean <- bcfishpass_phase2 %>%
   filter(stream_order != 1)
 
-wshds <- fpr_sp_watershed(bcfishpass_phase2_clean)
+wshds_raw <- fpr_sp_watershed(bcfishpass_phase2_clean)
+
+# calculate stats for each watershed
+wshds <- fpr::fpr_sp_wshd_stats(dat = wshds_raw) |>
+  mutate(area_km = round(area_ha/100, 1)) %>%
+  # below used to be mutate(across(contains('elev'), round, 0)) but throws error... complicated new format...
+  mutate(across(contains('elev'), \(x) round(x, 0))) %>%
+  arrange(stream_crossing_id)
 
 # ##add to the geopackage
 wshds %>%
-  sf::st_write(paste0("./data/fishpass_mapping/", 'fishpass_mapping', ".gpkg"), 'hab_wshds', append = F) ##might want to f the append....
+  sf::st_write("data/fishpass_mapping/fishpass_mapping.gpkg", 'hab_wshds', append = F) ##might want to f the append....
 
 #burn to kml as well so we can see elevations
 st_write(wshds, append = F, driver = 'kml', dsn = "data/inputs_extracted/wshds.kml")
 
+# put in the sqlite (really redundant but going to leave for now
+conn <- readwritesqlite::rws_connect("data/bcfishpass.sqlite")
+readwritesqlite::rws_write(wshds, exists = F, delete = TRUE,
+                           conn = conn, x_name = "wshds")
+readwritesqlite::rws_disconnect(conn)
+
 ####--------------------burn geojsons from geopackage-----------------------------------------------------
 ##we need geojsons to make the mapping convenient so lets pull everything out of the geopackage and write to geojson files
 read_gpkg <- function(layers = layer_name){
-  sf::st_read(dsn = "./data/fishpass_mapping/fishpass_mapping.gpkg", layer = layers) %>%
+  sf::st_read(dsn = "data/fishpass_mapping/fishpass_mapping.gpkg", layer = layers) %>%
     mutate(name = layers)
-    # sf::st_transform(crs = 4326)
 }
 
 ##get the names of the layers you want
-layer_names <- sf::st_layers(paste0("./data/fishpass_mapping/", 'fishpass_mapping', ".gpkg")) %>%
+layer_names <- sf::st_layers("data/fishpass_mapping/fishpass_mapping.gpkg") %>%
   pluck('name')
 
 ##grab the layers and give them a name
@@ -79,8 +75,6 @@ layers_to_burn <- layer_names %>%
   purrr::map(read_gpkg) %>%
   purrr::set_names(nm = layer_names)
 
-# ##now burn them to a folder called fishpass_mapping
-# dir.create('data/fishpass_mapping')
 
 write_geojson <- function(layers){
   layers %>%
